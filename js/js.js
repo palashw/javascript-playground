79a441708b454a05008149000d2761d775cf4f74
Palash <palashw173@gmail.com>
2018-07-06 11:25:14 +0530

    javacript vid 3

---
+++ js/js.js
@@ -0,0 +1,10 @@
+window.onload = function()
+    {
+        document.getElementById('mybutton').addEventListener
+        ('click', respond);
+    }
+
+    function respond(e)
+    {
+        alert("Thanks for clicking!");
+    }
